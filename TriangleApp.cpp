34bfdebb873cade43d391caf0d81b2edb974a8a5
Porrith Suong <psuong95@gmail.com>
2019-03-21 23:46:45 -0400

    Creates a VKInstance and checks if the instance is valid.

--- TriangleApp.cpp
+++ TriangleApp.cpp
@@ -22,7 +22,7 @@ namespace vulkan_rendering {
 	}
 
 	void TriangleApp::init_vulkan() {
-
+		create_instance();
 	}
 
 	void TriangleApp::main_loop() {
@@ -35,5 +35,31 @@ namespace vulkan_rendering {
 	void TriangleApp::cleanup() {
 		glfwDestroyWindow(window);
 		glfwTerminate();
+	}
+
+	void TriangleApp::create_instance() {
+		VkApplicationInfo app_info  = {};
+		app_info.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		app_info.pApplicationName   = "Triangle App";
+		app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		app_info.pEngineName        = "No Engine";
+		app_info.engineVersion      = VK_MAKE_VERSION(1, 0, 0);
+		app_info.apiVersion         = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo create_info = {};
+		create_info.sType                = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		create_info.pApplicationInfo     = &app_info;
+
+		uint32_t glfw_extension_count = 0;
+		const char** glfw_extension;
+
+		glfw_extension                      = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+		create_info.enabledExtensionCount   = glfw_extension_count;
+		create_info.ppEnabledExtensionNames = glfw_extension;
+		create_info.enabledLayerCount       = 0;
+
+		if (vkCreateInstance(&create_info, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("Failed to create VkInstance!");
+		}
 	}
 }
--- TriangleApp.h
+++ TriangleApp.h
@@ -2,6 +2,7 @@
 #define TRIANGLE_APP_H
 
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
 
 namespace vulkan_rendering {
 	
@@ -14,11 +15,13 @@ namespace vulkan_rendering {
 		const int WIDTH = 800;
 		const int HEIGHT = 600;
 		GLFWwindow* window;
+		VkInstance instance;
 
 		void init_window();
 		void init_vulkan();
 		void main_loop();
 		void cleanup();
+		void create_instance();
 	};
 }
 
