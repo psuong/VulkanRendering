dcd02b3d1ef62240aeabe243ea54d0e4f801313d
Porrith <psuong95@gmail.com>
2019-07-02 22:57:56 -0400

    Includes info about data layout of the example.

--- include/Vertex.h
+++ include/Vertex.h
@@ -3,12 +3,27 @@
 
 #include <glm/glm.hpp>
 #include <vector>
+#include <vulkan/vulkan.h>
 
 namespace vulkan_rendering {
 
     struct Vertex {
         glm::vec2 pos;
         glm::vec3 colour;
+
+        /**
+         * What does the vertex binding do?
+         *
+         * The vertex bindings describe the layout of the data. We can describe the # of bytes in between each entry and
+         * if the data should move to the next entry after each instance.
+         */
+        static VkVertexInputBindingDescription get_binding_descriptions() {
+            VkVertexInputBindingDescription bindings = {};
+
+            // TODO: Implement and describe the data layout.
+
+            return bindings;
+        }
     };
 
     const std::vector<Vertex> vertices = {
