89b75dc99310dca32517301d1d20a6112835cd1f
Porrith Suong <psuong95@gmail.com>
2019-09-16 23:46:29 -0400

    Updates the uniform buffer data coffee.

--- src/TriangleApp.cpp
+++ src/TriangleApp.cpp
@@ -1,4 +1,7 @@
-﻿#define GLFW_INCLUDE_VULKAN
+﻿#include "/usr/local/Cellar/glm/0.9.9.5/include/glm/ext/matrix_transform.hpp"
+#include "vulkan/vulkan_core.h"
+#include <cstring>
+#define GLFW_INCLUDE_VULKAN
 
 #include "../include/ExtensionValidation.h"
 #include "../include/QueueFamilyIndices.h"
@@ -1301,6 +1304,18 @@ namespace vulkan_rendering {
 
         auto current_time = std::chrono::high_resolution_clock::now();
         float time = std::chrono::duration<float, std::chrono::seconds::period>(current_time - start_time).count();
-        update_uniform_buffer(1);
+
+        UniformBufferObject ubo = {};
+        ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        ubo.proj = glm::perspective(glm::radians(45.0f), swap_chain_extent.width / (float) swap_chain_extent.height,
+            0.1f, 10.0f);
+
+        ubo.proj[1][1] *= -1;
+
+        void* data;
+        vkMapMemory(device, uniform_buffers_memory[current_img], 0, sizeof(ubo), 0, &data);
+        memcpy(data, &ubo, sizeof(ubo));
+        vkUnmapMemory(device, uniform_buffers_memory[current_img]);
     }
 }
